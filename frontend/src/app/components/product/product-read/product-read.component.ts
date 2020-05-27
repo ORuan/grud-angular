29c684e2bdace39213fb71d7579fda053b3d10e8
ORuan <ruanpablomp@gmail.com>
2020-05-27 07:44:57 -0300

    finalizado

--- frontend/src/app/components/product/product-read/product-read.component.ts
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -1,5 +1,6 @@
-import { ProductService } from './../product.service';
 import { Product } from './../product.modal';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,14 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductReadComponent implements OnInit {
   products: Product[]
-
+  displayedColumns = ['id', 'name', 'price', 'action']
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
     this.productService.read().subscribe(products => {
       this.products = products
-      console.log(products)
-    })
+    });
   }
-
+  
+  
 }
